b81d017374257eb7efb160aa8db9501d562da3c3
Hoanglovecode <van.hoang.10.05.2007@gmail.com>
2026-04-13 17:50:02 +0700

    Chung khoan VN

--- CTDLbasic/linklist/paper2.cpp
+++ CTDLbasic/linklist/paper2.cpp
@@ -1,38 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 struct node{
-    int data;
-    node* next;
-    node* pre;
+    int val;
+    node *next;
+    node *pre;
 };
-typedef struct node node;
-//create
-node *makenewnode(int x){
-    node *newnode=new node;
-    newnode->data=x;
+typedef struct node* node;
+node createnode(int x){
+    node newnode=new node;
+    newnode->val=x;
     newnode->next=NULL;
-    newnode->pre=NULL;
+    newnode->pre=nullptr;
     return newnode;
 }
-//count
-int count(node *head){
+int count(node head){
     int dem=0;
-    while(head->next!=NULL){
+    while(head->next){
         dem++;
         head=head->next;
     }
     return dem;
 }
-//review
-void review(node *head){
-    while(head->next!=NULL){
-        cout<<head->data<<' ';
+//duyet
+void duyet(node head){
+    if(head==nullptr)return;
+    while(head->next!=nullptr){
+        cout<<head->val<<' ';
         head=head->next;
     }
 }
 //pushfront
-void pushfront(node *&head,int x){
-    node *nodeA=makenewnode(x);
+void pushfront(node &head,int x){
+    node nodeA=createnode(x);
     nodeA->next=head;
     if(head!=NULL){
         head->pre=nodeA;
@@ -40,28 +39,29 @@ void pushfront(node *&head,int x){
     head=nodeA;
 }
 //pushback
-void pushback(node *&head,int x){
-    node *nodeB=makenewnode(x);
-    node *tmp=head;
-    if(head==NULL){
-        head=nodeB;return;
+void pushback(node &head,int x){
+    node nodeB=createnode(x);
+    if(head==nullptr){
+        head=nodeB;
+        return;
     }
-    while(tmp->next!=NULL){
+    node tmp=head;
+    while(tmp->next){
         tmp=tmp->next;
     }
     tmp->next=nodeB;
     nodeB->pre=tmp;
 }
 //insert
-void insert(node *&head,int x,int k){
-    node *nodeC=makenewnode(x);
-    node *tmp=head;
+void insert(node &head,int x,int k){
     int n=count(head);
-    if(k<1||k>n)return;
+    if(k<1||k>n+1)return;
     if(k==1){
         pushfront(head,x);
         return;
     }
+    node nodeC=createnode(x);
+    node tmp=head;
     for(int i=1;i<=k-1;i++){
         tmp=tmp->next;
     }
@@ -71,14 +71,12 @@ void insert(node *&head,int x,int k){
     tmp->pre=nodeC;
 }
 //popfront
-void popfront(node *&head,int x){
-    if(head==NULL)return;
-    node *tmp=head;
+void popfront(node &head){
+    if(head==nullptr)return;
+    node del=head;
     head=head->next;
-    if(head!=NULL){
-        head->pre=NULL;
-    }
-    delete(tmp);
+    if(head)head->pre=NULL;
+    delete(del);
 }
 //popback
 void popback(node &head){
@@ -116,9 +114,8 @@ void popmiddle(node &head,int k){
     }
     delete(del);
 }
-//delete node have value=x
-//Find max value node
-//FInd min value node
 int main(){
-   freopen("output.txt","w",stdout); 
+   freopen("input.txt","r",stdin);
+   freopen("output.txt","w",stdout);
+   
 }
